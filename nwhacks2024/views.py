11ab3c53c02cbef508ee86d7d7c558e57c2ae733
eerri <eerriikk@student.ubc.ca>
2024-01-20 20:38:24 -0800

    process_place_id finished

--- nwhacks2024/views.py
+++ nwhacks2024/views.py
@@ -1,21 +1,23 @@
-from django.shortcuts import render
+import json
+
 from django.conf import settings
 from django.http import JsonResponse
-import json
+from django.shortcuts import render
+from geooding.scrape_gmaps import get_photos_by_place_id
+
+# Create your views here.
 
 
 def load_map(request):
-    context = {
-        'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY
-    }
-    return render(request, 'home.html', context)
+    context = {"google_maps_api_key": settings.GOOGLE_MAPS_API_KEY}
+    return render(request, "home.html", context)
 
 
 def process_place_id(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = json.loads(request.body)
-        place_id = data.get('placeId')
+        place_id = data.get("placeId")
 
-        print(place_id)
+        photos = get_photos_by_place_id(place_id)
 
-        return JsonResponse({'status': 'success', 'message': 'Place ID processed'})
+        return JsonResponse({"status": "success", "message": "Place ID processed"})

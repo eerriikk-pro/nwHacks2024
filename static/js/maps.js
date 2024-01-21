d18f305e58c2af51a7d2f63b4134c391f612f4ea
kelly-olsson <kolsson2002@gmail.com>
2024-01-20 17:02:51 -0800

    Updated longitude/latitude

--- static/js/maps.js
+++ static/js/maps.js
@@ -15,7 +15,7 @@ window.onload = loadGoogleMapsApi;
 
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.866, lng: 151.196 },
+    center: { lat: 49.266, lng: -123.243 },
     zoom: 15,
   });
   const request = {
